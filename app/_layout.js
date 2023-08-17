f217277e15734789a0b498a474190ff7e504388b
Santiago Crippa <santicrippa@gmail.com>
2023-08-17 17:29:12 -0300

    uiux welcome screen

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,26 @@
-import { Stack } from 'expo-router'
+import { Stack } from 'expo-router';
+import { useCallback } from 'react';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
-    return <Stack />
+    const [fontsLoaded] = useFonts({
+        DMBold: require('../src/assets/fonts/DMSans-Bold.ttf'),
+        DMMedium: require('../src/assets/fonts/DMSans-Medium.ttf'),
+        DMRegular: require('../src/assets/fonts/DMSans-Regular.ttf'),
+    })
+
+    const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded) {
+            await SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded])
+
+    if (!fontsLoaded) return null
+
+    return <Stack onLayout={onLayoutRootView} />
 }
 
 export default Layout
