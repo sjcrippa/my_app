1fddaf3852f50679b94ebb4fa1472757871f7a9f
Santiago Crippa <santicrippa@gmail.com>
2023-08-24 15:50:18 -0300

    finishing ui/ux

--- src/components/home/popular/Popularjobs.jsx
+++ src/components/home/popular/Popularjobs.jsx
@@ -21,7 +21,11 @@ const Popularjobs = () => {
       num_pages: '1'
     })
 
-    console.log(data);
+    const [selectedJob, setSelectedJob] = useState()
+
+    const handleCardPress = (item) => {
+      
+    }
 
   return (
     <View style={styles.container}>
@@ -43,6 +47,8 @@ const Popularjobs = () => {
             renderItem={({ item }) => (
               <PopularJobCard
                 item={item}
+                selectedJob={selectedJob}
+                handleCardPress={handleCardPress}
               />
             )}
             keyExtractor={item => item?.job_id}
